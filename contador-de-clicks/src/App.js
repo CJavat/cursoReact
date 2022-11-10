7592e70de3fc10c41bd712fc3f2c55a564b8c668
Dani Plasc <cdpm98@hotmail.com>
2022-11-09 23:15:12 -0600

    Archivo hooks.txt agregado.

--- contador-de-clicks/src/App.js
+++ contador-de-clicks/src/App.js
@@ -1,40 +1,38 @@
-import './App.css';
-import freeCodeCampLogo from './img/freecodecamp-logo.png'
-import Boton from './components/boton'
-import Contador from './components/contador'
-import { useState } from 'react'
+import "./App.css";
+import freeCodeCampLogo from "./img/freecodecamp-logo.png";
+import Boton from "./components/boton";
+import Contador from "./components/contador";
+import { useState } from "react";
 
 function App() {
-  
   const [numeroClicks, setNumClicks] = useState(0);
 
   const manejarClick = () => {
     setNumClicks(numeroClicks + 1);
   };
-  
+
   const reiniciarContador = () => {
     setNumClicks(0);
   };
 
   return (
     <div className="App">
-      <div className='freecodecamp-logo-contenedor'>
-        <img 
-          className='freecodecamp-logo'
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
           src={freeCodeCampLogo}
-          alt='Logo de freeCodeCamp' />
-      </div>
-      <div className='contenedor-principal'>
-        <Contador
-          numeroClicks={ numeroClicks }
+          alt="Logo de freeCodeCamp"
         />
-        <Boton 
-          texto='Click'
+      </div>
+      <div className="contenedor-principal">
+        <Contador numeroClicks={numeroClicks} />
+        <Boton
+          texto="Click"
           esBotonDeClick={true}
           manejarClick={manejarClick}
         />
-        <Boton 
-          texto='Reiniciar'
+        <Boton
+          texto="Reiniciar"
           esBotonDeClick={false}
           manejarClick={reiniciarContador}
         />
